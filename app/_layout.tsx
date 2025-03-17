18a1367500b094270c097484af5e77b33eaf398f
3illBaby <thrillanon@outlook.com>
2025-03-17 18:28:33 +0100

    refactor(_layout.tsx): styled status bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,14 @@ import "./globals.css";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="(tabs)"
+        options={{
+          headerShown: false,
+          statusBarStyle: "light",
+          statusBarBackgroundColor: "#0f0d23",
+        }}
+      />
       <Stack.Screen name="movie/[id]" options={{ headerShown: false }} />
     </Stack>
   );
