f12a1fdf7fa4e78b964f8ce3916e8ffb27995767
3illBaby <thrillanon@outlook.com>
2025-03-26 17:19:32 +0100

    feat(appwrite.ts): update search count

---
+++ services/appwrtie.ts
@@ -0,0 +1,54 @@
+import { expoConfig } from "@/config/configs";
+import { Client, Databases, ID, Query } from "react-native-appwrite";
+
+interface IUpdateSearchCount {
+  query: string;
+  movie: Movie;
+}
+
+const client = new Client()
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject(expoConfig.getProjectId());
+
+const database = new Databases(client);
+
+export const updateSearchCount = async ({
+  query,
+  movie,
+}: IUpdateSearchCount) => {
+  const databaseId = expoConfig.getDatabaseId();
+  const collectionId = expoConfig.getCollectionId();
+
+  try {
+    const result = await database.listDocuments(databaseId, collectionId, [
+      Query.equal("search_term", query),
+    ]);
+
+    if (result.documents.length > 0) {
+      const exisitingMovie = result.documents[0];
+
+      await database.updateDocument(
+        databaseId,
+        collectionId,
+        exisitingMovie.$id,
+        {
+          count: exisitingMovie.count + 1,
+        },
+      );
+    } else {
+      await database.createDocument(databaseId, collectionId, ID.unique(), {
+        title: movie.title,
+        search_term: query,
+        movie_id: movie.id,
+        count: 1,
+        poster_url: `https://image.tmdb.org/t/p/w500/${movie.poster_path}`,
+      });
+    }
+  } catch (error: any) {
+    console.error(error);
+    return {
+      success: false,
+      error: error.message,
+    };
+  }
+};
